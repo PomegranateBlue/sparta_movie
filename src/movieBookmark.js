9132eb147f6e533c51721bf229ab743aa8b0fa89
bluepome98 <wpdn1998@gmail.com>
2025-01-16 12:58:39 +0900

    북마크 저장 및 렌더링까지 완료

--- src/movieBookmark.js
+++ src/movieBookmark.js
@@ -1,7 +1,49 @@
-const bookmarkBtn = document.querySelector("#bookmarkPage");
-const bookmarkContainer = document.querySelector(".movieContainer");
-const addBookmark = document.querySelector("bookmarkBtn");
-bookmarkBtn.addEventListener("click", function (e) {
-  bookmarkContainer.innerHTML = "";
+
+
+let bookmarkContainer = document.querySelector(".movieContainer");
+const addBookmark = document.querySelector("#bookmarkBtn");
+const bookmarkPage = document.querySelector("#bookmarkPage");
+const saveMovieToLocal = () => {
+  const bookmarkList =
+    JSON.parse(localStorage.getItem("bookmarkedMovie")) || [];
+  const bookmarkData = {
+    poster: document.querySelector("#modalMoviePoster img").src,
+    title: document.querySelector("#modalMovieTitle").textContent,
+    overview: document.querySelector("#modalMovieOverview").textContent,
+    rating: document.querySelector("#modalMovieRating").textContent,
+    release: document.querySelector("#modalMovieRelease").textContent,
+  };
+  bookmarkList.push(bookmarkData);
+  localStorage.setItem("bookmarkedMovie", JSON.stringify(bookmarkList));
+};
+
+addBookmark.addEventListener("click", function (e) {
   console.log(e.target);
+  saveMovieToLocal();
 });
+
+const renderBookmarkList = () => {
+  const loadLocalStorage = JSON.parse(localStorage.getItem("bookmarkedMovie"));
+  bookmarkContainer.innerHTML = "";
+  console.log(loadLocalStorage);
+  //bookmarkContainer -> 현재 화면에 보여지는 영화카드 지우고 렌더링하기
+  // let tempHtml = `<div class="movieCard">
+  //   <div id="movieImage"><img src=https://image.tmdb.org/t/p/w342${movieImage}></div>
+  //   <div id="movieTitle">${movieTitle}</div>
+  //   <div id="movieRating">${movieRating}</div>
+  //   <div data-id="${movieId}"></div>
+  //   <div data-overview="${movieOverview}"></div>
+  //   <div data-release="${movieRelease}"></div>
+  // </div>`;
+  loadLocalStorage.forEach((data) => {
+    let temp = `<div class="movieCard">
+    <div id="movieImage"><img src=${data.poster}></div>
+    <div id="movieTitle">${data.title}</div>
+    <div id="movieRating">${data.rating}</div>
+  </div>`;
+    bookmarkContainer.innerHTML += temp;
+  });
+};
+
+bookmarkPage.addEventListener("click", renderBookmarkList);
+renderBookmarkList();
